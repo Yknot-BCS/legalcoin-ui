e9f1e1e5f1b82efba01e7db02523e029464ae209
Ja-Q <jpbeukes27@gmail.com>
2022-05-13 17:32:21 +0200

    feat: add headers to accounts graphql requests

--- src/api/accounts.ts
+++ src/api/accounts.ts
@@ -1,18 +1,23 @@
 /* eslint-disable */
 import axios from 'axios';
 
+// TODO request error handling
+// TODO include bearer token in headers
+const headers = {
+    'Content-Type': 'application/json',
+}
+
 const query = async function (query: string): Promise<unknown>{
   return await axios.post(process.env.ACCOUNTS_API_ENDPOINT, {
     query,
     variables: null
-  });
+  }, { headers });
 };
 const mutation = async function (query: string): Promise<unknown>{
-    const res = await axios.post(process.env.ACCOUNTS_API_ENDPOINT, {
+    return await axios.post(process.env.ACCOUNTS_API_ENDPOINT, {
       query: `mutation { ${query} }`,
       variables: null
-    });
-    return res.data;
+    }, { headers });
   };
 
 export default {
