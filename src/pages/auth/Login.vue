e0229aaf19d29d8340250fb6fbf3d98ed2045c8f
JPBeukes <jpbeukes27@gmail.com>
2022-05-21 14:33:03 +0200

    feat: add skip link to login

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -36,46 +36,51 @@ export default {
         }
       }
     };
-  },
-  components: { UalLoginHandler }
+  }
 };
 </script>
 
 <template lang="pug">
-.form-wrapper.row.q-pa-md.bg-grey-3
-  .text-h5.col-12 Login
-  q-form(@submit="onSubmit").col-12.row.q-mb-sm.q-gutter-y-lg
-    q-input(
-        v-model="userEmail"
-        label="Email"
-        lazy-rules
-        :rules="[ requiredRule ]"
-        autocomplete="email"
-    ).col-12
-    q-input(
-        v-model="userPassword"
-        type="password"
-        label="Password"
-        lazy-rules
-        :rules="[ requiredRule ]"
-        autocomplete="current-password"
-    ).col-12
-    q-btn(type="submit" color="primary").col-12 Login
+.page-wrapper
+  .row.q-pa-md.bg-grey-3
+    .text-h5.col-12 Login
+    q-form(@submit="onSubmit").col-12.row.q-mb-sm.q-gutter-y-lg
+      q-input(
+          v-model="userEmail"
+          label="Email"
+          lazy-rules
+          :rules="[ requiredRule ]"
+          autocomplete="email"
+      ).col-12
+      q-input(
+          v-model="userPassword"
+          type="password"
+          label="Password"
+          lazy-rules
+          :rules="[ requiredRule ]"
+          autocomplete="current-password"
+      ).col-12
+      q-btn(type="submit" color="primary").col-12 Login
 
-    UalLoginHandler
-
-  .col-12.text-center
-    span.q-mr-xs Need an account?
-    router-link(to="register") Register
-  .col-12.text-center
-    span.q-mr-xs Forgot your password?
-    router-link(to="resetpassword") Reset Password
+    .col-12.text-center
+      span.q-mr-xs Forgot your password?
+      router-link(to="resetpassword") Reset Password
+    .col-12.text-center
+      span.q-mr-xs Need an account?
+      router-link(to="register") Register
+  .row.justify-center.align-center.q-my-lg
+    router-link(:to="{name:'home'}").skip-link Skip this step
+      
 
 </template>
 
 <style lang="sass" scoped>
-.form-wrapper
+.page-wrapper
   flex-basis: 600px
   flex-grow: 0
   flex-shrink: 1
+.skip-link
+  text-decoration: none
+  font-style: italic
+  color: black
 </style>
