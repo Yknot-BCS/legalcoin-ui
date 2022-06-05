52d5e2dbfa051a3caff60c104575a2c28b83a9a2
Ja-Q <jpbeukes27@gmail.com>
2022-06-05 18:33:47 +0200

    feat: platform signer get private key

--- src/components/auth/PlatformSigner.vue
+++ src/components/auth/PlatformSigner.vue
@@ -1,21 +1,51 @@
 <script lang="ts">
 import { useStore } from 'src/store';
-import { defineComponent, computed } from 'vue';
+import { useQuasar } from 'quasar';
+import { defineComponent, computed, ref } from 'vue';
+import { api } from 'src/api';
+import { requiredRule } from '../../pages/auth/inputRules';
 export default defineComponent({
   setup() {
     const store = useStore();
+    const $q = useQuasar();
     const showSignModal = computed(
       () => store.state.account.showPlatformSigner
     );
     const cryptoAccount = computed(
       () => store.state.account.profile.cryptoAccount
     );
+    const userPassword = ref('');
     const closeModal = () =>
       store.commit('account/setShowPlatformSigner', false);
+    const getPrivateKey = async (): Promise<string> => {
+      try {
+        type CryptoPrivateKeyResponse = {
+          cryptoPrivateKey: string;
+        };
+        const res = (await api.accounts.query(`
+          {
+            cryptoPrivateKey(input: {password: "${userPassword.value}"})
+          }
+        `)) as CryptoPrivateKeyResponse;
+        return res.cryptoPrivateKey;
+      } catch (error) {
+        $q.notify({
+          type: 'negative',
+          message: (error as Error).message
+        });
+      }
+    };
+    const sendTransaction = async () => {
+      const privateKey = await getPrivateKey();
+    };
     return {
       showSignModal,
       cryptoAccount,
-      closeModal
+      userPassword,
+      closeModal,
+      getPrivateKey,
+      sendTransaction,
+      requiredRule
     };
   }
 });
@@ -28,9 +58,19 @@ q-dialog(v-model='showSignModal')
       .text-h5 Confirm Transaction
     q-card-section
       div Signing with {{ cryptoAccount.accountName }}
+      div Please enter your password to confirm the transaction.
+    q-card-section
+      q-input(
+        v-model='userPassword',
+        type='password',
+        label='Password',
+        lazy-rules,
+        :rules='[requiredRule]',
+        autocomplete='current-password'
+      )
     q-card-actions(align='right')
       q-btn(@click='closeModal()') Cancel
-      q-btn(@click='closeModal()', color='primary') Confirm
+      q-btn(@click='sendTransaction()', color='primary') Confirm
 </template>
 
 <style lang="sass">
