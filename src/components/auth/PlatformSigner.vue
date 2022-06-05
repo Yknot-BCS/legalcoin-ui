53aa082ebb338aae5a04029428ead92c8993973d
Ja-Q <jpbeukes27@gmail.com>
2022-06-05 16:59:52 +0200

    style: profile signer

--- src/components/auth/PlatformSigner.vue
+++ src/components/auth/PlatformSigner.vue
@@ -7,10 +7,14 @@ export default defineComponent({
     const showSignModal = computed(
       () => store.state.account.showPlatformSigner
     );
+    const cryptoAccount = computed(
+      () => store.state.account.profile.cryptoAccount
+    );
     const closeModal = () =>
       store.commit('account/setShowPlatformSigner', false);
     return {
       showSignModal,
+      cryptoAccount,
       closeModal
     };
   }
@@ -20,9 +24,16 @@ export default defineComponent({
 <template lang="pug">
 q-dialog(v-model='showSignModal')
   q-card
-    q-card-section Platform Signer
-    q-card-actions 
+    q-card-section
+      .text-h5 Confirm Transaction
+    q-card-section
+      div Signing with {{ cryptoAccount.accountName }}
+    q-card-actions(align='right')
       q-btn(@click='closeModal()') Cancel
+      q-btn(@click='closeModal()', color='primary') Confirm
 </template>
 
-<style lang="sass"></style>
+<style lang="sass">
+.q-card
+  flex-basis: 400px
+</style>
