fda1122663dccf5b26a75b1278fafaf0d38256f0
Ja-Q <jpbeukes27@gmail.com>
2022-06-06 00:01:34 +0200

    feat: create transfer tokens example form

--- src/pages/ProfileSettings.vue
+++ src/pages/ProfileSettings.vue
@@ -1,68 +1,24 @@
 <script lang="ts">
 import { defineComponent, ref, computed } from 'vue';
 import { Computed, mapGetters } from 'vuex';
-import { useStore } from 'src/store';
-import { useQuasar } from 'quasar';
-import { AnyAction } from '@greymass/eosio';
-import { SignTransactionResponse } from 'universal-authenticator-library';
+// import { useStore } from 'src/store';
+// import { useQuasar } from 'quasar';
 import LinkCryptoAccount from 'src/components/auth/LinkCryptoAccount.vue';
 import NewCryptoAccount from 'src/components/auth/NewCryptoAccount.vue';
+import TransferTokensForm from 'src/components/auth/TransferTokensForm.vue';
 
 export default defineComponent({
   name: 'Template',
-  components: { LinkCryptoAccount, NewCryptoAccount },
+  components: { LinkCryptoAccount, NewCryptoAccount, TransferTokensForm },
   computed: {
     ...mapGetters({
       account: 'account/account'
     })
   },
   setup() {
-    const SYS = computed(() => process.env.NETWORK_TOKEN);
-    const store = useStore();
-    const $q = useQuasar();
-    const cryptoAccountName = computed(
-      () => store.state.account.profile.cryptoAccount.accountName
-    );
-    async function send() {
-      const action = {
-        account: 'eosio.token',
-        name: 'transfer',
-        authorization: [
-          { actor: cryptoAccountName.value, permission: 'active' }
-        ],
-        data: {
-          from: cryptoAccountName.value,
-          to: 'zzzzzzzzb.ya',
-          quantity: `1.0000 ${SYS.value}`,
-          memo: 'Test platform signer'
-        }
-      } as AnyAction; // TODO cast to modified AnyAction (create new type) (auth not needed)
-
-      try {
-        const res = (await store.dispatch('account/sendTransaction', {
-          actions: [action]
-        })) as SignTransactionResponse;
-        console.log(res.transactionId);
-        $q.notify({
-          type: 'positive',
-          message: 'Transaction signed'
-        });
-        $q.dialog({
-          title: 'Transaction signed',
-          message: `Transaction ID: ${res.transactionId}`
-        });
-      } catch (error) {
-        $q.notify({
-          type: 'negative',
-          message: (error as Error).message
-        });
-      }
-    }
     return {
       hideProfile: ref(false),
-      emailNotifications: ref(false),
-      send,
-      SYS
+      emailNotifications: ref(false)
     };
   }
 });
@@ -111,11 +67,9 @@ q-page.fit.row.wrap.justify-center
     q-separator
     q-card-section
       NewCryptoAccount
-    q-separator 
-    q-card-section 
-      .text-bold Transfer example
-      div Transfer 1.0000 {{ SYS }} to zzzzzzzzb.ya
-      q-btn(@click='send()') Send
+    q-separator
+    q-card-section
+      TransferTokensForm
 </template>
 
 <style lang="sass" scoped>
