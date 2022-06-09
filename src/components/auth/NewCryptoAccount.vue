5d6dbf90028d2ba3da494ff7423d3ecd592d5d2f
Ja-Q <jpbeukes27@gmail.com>
2022-06-09 08:07:48 +0200

    feat: Add reset crypto account

--- src/components/auth/NewCryptoAccount.vue
+++ src/components/auth/NewCryptoAccount.vue
@@ -61,10 +61,40 @@ export default defineComponent({
     async function createNewAccount() {
       await cryptoNew();
     }
+    function cryptoReset() {
+      $q.dialog({
+        title: 'Crypto Account Reset',
+        message:
+          'Are you sure you want to reset your crypto account? This will only affect your profile, not the blockchain, but you may loose access to your account.',
+        cancel: true,
+        focus: 'cancel',
+        color: 'negative'
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      }).onOk(async () => {
+        try {
+          await api.accounts.mutation(`
+            {
+              cryptoReset
+            }
+            `);
+          await store.dispatch('account/refreshProfile');
+          $q.notify({
+            type: 'positive',
+            message: 'Reset crypto account'
+          });
+        } catch (error) {
+          $q.notify({
+            type: 'negative',
+            message: (error as Error).message
+          });
+        }
+      });
+    }
     return {
       cryptoIsAuthenticated,
       profileCryptoAccount,
       userPassword,
+      cryptoReset,
       connectExistingAccount,
       createNewAccount,
       requiredRule
@@ -77,10 +107,16 @@ export default defineComponent({
 .q-gutter-y-sm
   .col-12.text-bold Profile Crypto Account
   .col-12.text-italic With this account you can sign transactions on the blockchain without connecting an independant wallet (like Anchor).
-  .col-12(v-if='profileCryptoAccount.accountName !== ""') Profile account name: &nbsp;
-    span.text-bold {{ profileCryptoAccount.accountName }}
-  .col-12(v-if='profileCryptoAccount.accountName !== ""') Profile public key: &nbsp;
-    span.text-bold {{ profileCryptoAccount.publicKey }}
+  .col-12.row(v-if='profileCryptoAccount.accountName !== ""')
+    .col-12 Profile account name: &nbsp;
+      span.text-bold {{ profileCryptoAccount.accountName }}
+    .col-12 Profile public key: &nbsp;
+      span.text-bold {{ profileCryptoAccount.publicKey }}
+    q-btn.col-12(
+      @click='cryptoReset',
+      color='negative',
+      label='Reset crypto account'
+    ) 
   q-card(flat, bordered)(v-else)
     q-item.bg-info You still need to create a crypto account for your profile. Connect an existing account or create a new one below.
     q-card-section
