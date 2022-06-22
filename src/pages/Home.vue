c37c24fc5b862909f2aa1c517c67cc6ac9098879
kobus <kobusvdwalt9@gmail.com>
2022-06-22 21:41:44 +0200

    chore: first pass at home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,40 +18,65 @@ export default {
 </script>
 
 <template lang="pug">
-img.bg.tr.animated.fadeInLeft.slower(src='~assets/polygons/v1.svg')
-img.bg-polygon.tr.animated.fadeInLeft.slower(src='~assets/polygons/v2.svg')
-q-page.q-py-md
-  .row
-    .col-md-6.row.justify-end.items-center.q-px-lg
-      h2.text-grey-1 Bringing the legal industry into the digital world
-    .main-asset.col-md-6.q-pa-lg(v-if='assets.length > 0')
-      Cards.rounded.shadow-10(:data='assets[0]', type='asset')
+.row
+  .col-12.col-md-6
+    .landing-left
+      h2.text-grey-9 Fund some of the most worthy Class Action Lawsuits with LegalCoin.
+      p.landing-subheading Fiving you access to the justice you deserve. Play a role in bringing large conglomorates to justice by funding cases such as the diesel emissions scandal.
+  .col-12.col-md-6.pgb
+    .landing-right
+      h4.q-ma-none.q-pl-lg.trending-title Trending
+      .landing-right-card-container
+        .main-asset.col-md-6.q-pa-lg(v-if='assets.length > 0')
+          Cards.shadow-10(:data='assets[0]', type='asset')
 </template>
 
 <style lang="sass" scoped>
-h2
-  max-width: 21ch
-.main-asset
-  flex-basis: 600px
+.pgb
+  position: relative
 
-.rounded
-  border-radius: 1rem
-  overflow: hidden
+@media (min-width: $breakpoint-sm-max)
+  .landing-heading
+    color: grey-10
+  .landing-subheading
+    font-family: "ralewayregular"
+    font-size: 1.5rem
+  .landing-left
+    padding: 4rem
+    text-align: center
+    max-width: 45rem
+    margin-left:2rem
 
-.bg
-  position: absolute
-  height: 100%
-  left: 0rem
-  z-index: -1
-  &.tr
-    right: 0rem
+  .landing-right
+    padding: 5rem
+  .landing-right-card-container
+    max-width:40rem
+  .trending-title
+    font-weight: 200
+
+  .pgb::before
+    content:' '
+    position: absolute
+    z-index: -1
     top: 0rem
-  &.br
-    left: 0rem
+    left: -15rem
+    right: 0rem
     bottom: 0rem
-
-.bg-polygon
-  position: absolute
-  left: 3rem
-  bottom: 10rem
+    background-image: url("~assets/polygons/polygon_home_trending_horizontal.svg")
+    background-repeat: no-repeat
+    background-size: cover
+@media (max-width: $breakpoint-md-min)
+  .q-pa-xl
+    padding: 1rem
+  .pgb::before
+    content:' '
+    position: absolute
+    z-index: -1
+    top: -3rem
+    left: 0rem
+    right: 0rem
+    bottom: 0rem
+    background-image: url("~assets/polygons/polygon_home_trending_vertical.svg")
+    background-repeat: no-repeat
+    background-size: cover
 </style>
