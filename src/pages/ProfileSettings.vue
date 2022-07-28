c6e43c4c8c4d9d66677d02cc4997a4db8499cdf6
Th3Doo <scottiec4rock@gmail.com>
2022-07-28 12:17:53 +0200

    fix: profile settings banner color

--- src/pages/ProfileSettings.vue
+++ src/pages/ProfileSettings.vue
@@ -39,7 +39,7 @@ export default defineComponent({
 
 <template lang="pug">
 q-card(flat, square)
-  q-card-section.row.bg-primary.items-center.content-center.justify-center
+  q-card-section.row.bg-white.items-center.content-center.justify-center
     .col-2
       q-btn(icon='fas fa-arrow-left', round, flat, onclick='history.go(-1)')
     .col-8.text-body1.text-uppercase.text-center.self-center Profile settings
