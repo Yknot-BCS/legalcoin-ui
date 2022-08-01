0ea72eb117d2e7dac7ab654b47a26df01af802c8
Th3Doo <scottiec4rock@gmail.com>
2022-08-01 08:37:07 +0200

    feat: added flip-side to template cards

--- src/components/gallery/cards/templateCard.vue
+++ src/components/gallery/cards/templateCard.vue
@@ -44,8 +44,6 @@ export default defineComponent({
     changeCard() {
       this.viewCard = !this.viewCard;
       this.viewInfo = !this.viewInfo;
-      console.log(this.viewCard);
-      console.log(this.viewInfo);
       this.$forceUpdate();
     }
   }
@@ -53,82 +51,10 @@ export default defineComponent({
 </script>
 
 <template lang="pug">
-q-card(v-if='viewCard')
-  //- Desktop
-  q-card-section.bg-white(style='z-index: 2, height: 3rem')(v-if='$q.screen.gt.sm')
-    .row.col.justify-between
-      .col-8
-        //- Title of cards
-        .row
-          .text-subtitle2.heading {{ card.name }}
-          q-btn(color='red', @click='changeCard()', size='sm', flat)
-            | ?
-      .col-2
-        q-badge.q-mb-xs.text-subtitle2.float-right(
-          rounded,
-          :color='badgeColour'
-        ) {{ card.tier }}
-    //- Large cards details
-    .row(style='font-size: 0.7rem')
-      .col-8
-        .col-auto.text-bold
-          | Price:
-        .col-auto.values
-          img.q-pr-xs(
-            src='~assets/legalcoin-trimmed.png',
-            style='height: 10px'
-          )
-          | {{ getPrice }}
-      .col
-        .col-auto.text-bold
-          .text-right Yield:
-        .col-auto.float-right.values
-          .text-subtitle4 {{ card.yield }}
-
-  //- Mobile
-  q-card-section.bg-white(style='z-index: 2')(v-else)
-    .row.justify-between
-      .col-8
-        //- Title of cards
-        .row
-          .text-subtitle1.heading {{ card.name }}
-          q-btn(color='red', @click='changeCard()', size='sm', flat)
-            | ?
-      .col-2.self-center
-        q-badge.text-subtitle2.float-right(rounded, :color='badgeColour')
-    //- Small cards details
-    .row(style='font-size: 0.7rem')
-      .col-10
-        .col-auto.text-bold
-          | Price:
-        .col-auto
-          img.q-pr-sm(
-            src='~assets/legalcoin-trimmed.png',
-            style='height: 10px'
-          )
-          | {{ getPrice }}
-      .col
-        .col-auto.text-bold
-          .text-right Yield:
-        .col-auto.float-right
-          .text-subtitle4 {{ card.yield }}
-
-  router-link(
-    :to='{ name: "template", params: { collection_name: card.collection, template_id: card.id } }'
-  )
-    q-img.asset-img-lg.zoom(v-if='$q.screen.gt.sm', :src='card.imageUrl')
-    q-img.asset-img-md.zoom(
-      v-if='$q.screen.lt.md && $q.screen.gt.xs',
-      :src='card.imageUrl'
-    )
-    q-img.asset-img-sm.zoom(
-      v-if='$q.screen.lt.sm',
-      :src='card.imageUrl'
-    )
-q-card(v-if='viewInfo')
-  //- Desktop
-  .div(v-if='$q.screen.gt.sm')
-    q-card-section.bg-white(style='z-index: 2')
+transition(v-if='viewCard', appear, enter-to-class='animated flipInY' :duration='1000')
+  q-card
+    //- Desktop
+    q-card-section.bg-white(style='z-index: 2, height: 3rem')(v-if='$q.screen.gt.sm')
       .row.col.justify-between
         .col-8
           //- Title of cards
@@ -157,33 +83,9 @@ q-card(v-if='viewInfo')
             .text-right Yield:
           .col-auto.float-right.values
             .text-subtitle4 {{ card.yield }}
-    q-card-section.asset-img-lg
-      .div
-        .row.heading
-          | Details
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Max Supply:
-          .col-auto
-            | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Issued Supply:
-          .col-auto
-            | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Is Transferable:
-          .col-auto
-            | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Is Burnable:
-          .col-auto
-          | ???
-  //- Mobile
-  .div(v-else)
-    q-card-section.bg-white(style='z-index: 2')
+
+    //- Mobile
+    q-card-section.bg-white(style='z-index: 2')(v-else)
       .row.justify-between
         .col-8
           //- Title of cards
@@ -209,54 +111,152 @@ q-card(v-if='viewInfo')
             .text-right Yield:
           .col-auto.float-right
             .text-subtitle4 {{ card.yield }}
-    q-card-section.asset-img-md(v-if='$q.screen.lt.md && $q.screen.gt.xs')
-      .div
-        .row.heading
-          | Details
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Max Supply:
-          .col-auto
+
+    router-link(
+      :to='{ name: "template", params: { collection_name: card.collection, template_id: card.id } }'
+    )
+      q-img.asset-img-lg.zoom(v-if='$q.screen.gt.sm', :src='card.imageUrl')
+      q-img.asset-img-md.zoom(
+        v-if='$q.screen.lt.md && $q.screen.gt.xs',
+        :src='card.imageUrl'
+      )
+      q-img.asset-img-sm.zoom(
+        v-if='$q.screen.lt.sm',
+        :src='card.imageUrl'
+      )
+transition(v-if='viewInfo', appear, enter-to-class='animated flipInY', leave-to-class='animated flipOutY')
+  q-card
+    //- Desktop
+    .div(v-if='$q.screen.gt.sm')
+      q-card-section.bg-white(style='z-index: 2')
+        .row.col.justify-between
+          .col-8
+            //- Title of cards
+            .row
+              .text-subtitle2.heading {{ card.name }}
+              q-btn(color='red', @click='changeCard()', size='sm', flat)
+                | ?
+          .col-2
+            q-badge.q-mb-xs.text-subtitle2.float-right(
+              rounded,
+              :color='badgeColour'
+            ) {{ card.tier }}
+        //- Large cards details
+        .row(style='font-size: 0.7rem')
+          .col-8
+            .col-auto.text-bold
+              | Price:
+            .col-auto.values
+              img.q-pr-xs(
+                src='~assets/legalcoin-trimmed.png',
+                style='height: 10px'
+              )
+              | {{ getPrice }}
+          .col
+            .col-auto.text-bold
+              .text-right Yield:
+            .col-auto.float-right.values
+              .text-subtitle4 {{ card.yield }}
+      q-card-section.asset-img-lg
+        .div
+          .row.heading
+            | Details
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Max Supply:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Issued Supply:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Is Transferable:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Is Burnable:
+            .col-auto
             | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Issued Supply:
-          .col-auto
+    //- Mobile
+    .div(v-else)
+      q-card-section.bg-white(style='z-index: 2')
+        .row.justify-between
+          .col-8
+            //- Title of cards
+            .row
+              .text-subtitle1.heading {{ card.name }}
+              q-btn(color='red', @click='changeCard()', size='sm', flat)
+                | ?
+          .col-2.self-center
+            q-badge.text-subtitle2.float-right(rounded, :color='badgeColour')
+        //- Small cards details
+        .row(style='font-size: 0.7rem')
+          .col-10
+            .col-auto.text-bold
+              | Price:
+            .col-auto
+              img.q-pr-sm(
+                src='~assets/legalcoin-trimmed.png',
+                style='height: 10px'
+              )
+              | {{ getPrice }}
+          .col
+            .col-auto.text-bold
+              .text-right Yield:
+            .col-auto.float-right
+              .text-subtitle4 {{ card.yield }}
+      q-card-section.asset-img-md(v-if='$q.screen.lt.md && $q.screen.gt.xs')
+        .div
+          .row.heading
+            | Details
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Max Supply:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Issued Supply:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Is Transferable:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Is Burnable:
+            .col-auto
             | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Is Transferable:
-          .col-auto
+      q-card-section.asset-img-sm(v-if='$q.screen.lt.sm')
+        .div
+          .row.heading
+            | Details
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Max Supply:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Issued Supply:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Is Transferable:
+            .col-auto
+              | ???
+          .row.justify-between.q-pb-sm
+            .col-auto
+              | Is Burnable:
+            .col-auto
             | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Is Burnable:
-          .col-auto
-          | ???
-    q-card-section.asset-img-sm(v-if='$q.screen.lt.sm')
-      .div
-        .row.heading
-          | Details
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Max Supply:
-          .col-auto
-            | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Issued Supply:
-          .col-auto
-            | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Is Transferable:
-          .col-auto
-            | ???
-        .row.justify-between.q-pb-sm
-          .col-auto
-            | Is Burnable:
-          .col-auto
-          | ???
 </template>
 
 <style lang="sass" scoped>
@@ -275,4 +275,11 @@ q-card(v-if='viewInfo')
   font-weight: bold
 .values
   font-size: 1rem
+.v-enter-active,
+.v-leave-active
+  transition: opacity 0.5s ease
+
+.v-enter-from,
+.v-leave-to
+  opacity: 0
 </style>
