d4f0b8a68f7a1b129c1ccf426e49770b3399717d
Th3Doo <scottiec4rock@gmail.com>
2022-08-26 13:29:55 +0200

    feat: added link to legalcoin.com

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -19,6 +19,9 @@ export default {
   .row.justify-center.q-gutter-x-lg
     .col-12.col-md-2.section
       h4 Contact Us
+      p.sub-heading Website:
+      p.text-grey-6
+        a.text-grey-6(href='https://www.legalcoin.com/', target='_blank') legalcoin.com
       p.sub-heading Location:
       p.text-grey-6
         | Floor 2
