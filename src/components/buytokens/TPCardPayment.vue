da0ba41ea706fcf60140f7db19f32b510636107d
Dewald928 <dewald123@rocketmail.com>
2022-10-17 16:58:14 +0200

    feat: payment working. make prettier

---
+++ src/components/buytokens/TPCardPayment.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
+import axios from 'axios';
+import { date } from 'quasar';
+import { mapGetters } from 'vuex';
+import crypto from 'crypto';
+import { required } from 'yargs';
+
+export default defineComponent({
+  name: 'TPCardPayment',
+  props: {
+    paymentId: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  setup() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      accountName: 'account/getAccountName',
+      isAuthenticated: 'account/isAuthenticated'
+    }),
+    currentUrl(): string {
+      return window.location.origin;
+    }
+  },
+  methods: {},
+  watch: {
+    paymentId: function (newVal, oldVal) {
+      console.log('paymentId changed');
+      console.log('newVal: ', newVal);
+      console.log('oldVal: ', oldVal);
+    }
+  },
+  mounted() {
+    let url = process.env.DEVELOPMENT
+      ? 'https://eu-test.oppwa.com'
+      : 'https://eu-prod.oppwa.com';
+    let elementExists = document.getElementById('tpScript');
+    if (this.paymentId !== '' && !elementExists) {
+      let tpScript = document.createElement('script');
+      tpScript.id = 'tpScript';
+      tpScript.setAttribute(
+        'src',
+        `${url}/v1/paymentWidgets.js?checkoutId=${this.paymentId}`
+      );
+      document.head.appendChild(tpScript);
+    }
+  }
+});
+</script>
+
+<template lang="pug">
+form.paymentWidgets(
+  action=`${currentUrl}/buytokens/success`,
+  data-brands='VISA MASTER AMEX'
+)
+</template>
+
+<style lang="sass" scoped></style>
